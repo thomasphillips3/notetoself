3e11a9f2bf5a9775098ee9d0e57e49fab8cb2d7e
Thomas Phillips <thomasphillips3@gmail.com>
2018-08-07 18:58:01 -0700

    More App component tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,4 +8,23 @@ describe('App', () => {
     it('renders the title', () => {
         expect(app.find('h2').text()).toEqual('Note to Self');
     });
+
+    it('renders the clear button', () => {
+        expect(app.find('.btn').at(1).text()).toEqual('Delete all Notes');
+    });
+    
+    describe('when rendering the form', () => {
+        it('renders a Form component', () => {
+            expect(app.find('Form').exists()).toBe(true);
+        });
+
+        it('renders a FormControl component', () => {
+            expect(app.find('FormControl').exists()).toBe(true);
+        });
+
+        it('renders a submit button', () => {
+            expect(app.find('.btn').at(0).text()).toEqual('Submit');
+        });
+    });
 });
+
