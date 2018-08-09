2e7b14b16f0b4ab576fe90b7aeb4ac0db7372ae1
Thomas Phillips <thomasphillips3@gmail.com>
2018-08-08 21:54:26 -0700

    Test the cookie

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -45,8 +45,25 @@ describe('App', () => {
                 app.find('.btn').at(0).simulate('click');
             });
 
+            afterEach(() => {
+                app.find('.btn').at(1).simulate('click');
+            });
+            
             it('adds the new note to state', () => {
                 expect(app.state().notes[0].text).toEqual(testNote);
+            });
+
+            describe('and remounting the component', () => {
+                let app2;
+
+                beforeEach(() => {
+                    app2 = mount(<App />);
+                });
+
+
+                it('reads the stored note cookie', () => {
+                    expect(app2.state().notes).toEqual([{ text: testNote }]);
+                });
             });
 
             describe('and clicking the clear button', () => {
